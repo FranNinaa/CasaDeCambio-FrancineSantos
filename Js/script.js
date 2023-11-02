5e855a1f7706a04c3f6f933b01682d5d0bb0e4b1
FranNinaa <francinesantoss85@gmail.com>
2023-11-02 12:30:54 -0300

    implementado a funcionalidade de mostrar a lista de moedas nos inputs de moeda origem e moeda destino

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,13 @@
-// Função para carregar a lista de moedas da origem
-function carregarMoedas() {
-    // Obtém o elemento de entrada "Moeda de Origem" e o elemento datalist correspondente
-    let moedaOrigemInput = document.getElementById('moedaOrigemInput');
-    let datalist = document.getElementById('idMoedaOrigem');
+// Função para carregar a lista de moedas (tanto origem quanto destino)
+function carregarMoedas(datalistId) {
+    // Obtém o elemento de entrada (moedaOrigemInput ou moedaDestinoInput) e o elemento datalist correspondente
+    let inputElement = document.getElementById(datalistId);
+    let datalist = document.getElementById(datalistId);
 
     // Cria uma nova solicitação XMLHttpRequest
     let xhr = new XMLHttpRequest();
 
-    // Abre uma solicitação GET para a API do Banco Central
+    // Abre uma solicitação GET para a API do Banco Central (Note que você pode enfrentar problemas com chamadas de API para outro domínio)
     xhr.open('GET', 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json&$select=simbolo,nomeFormatado');
 
     // Adiciona um evento para lidar com a resposta da solicitação
@@ -16,6 +16,9 @@ function carregarMoedas() {
         if (xhr.status === 200) {
             // Analisa os dados JSON da resposta
             let moedas = JSON.parse(xhr.responseText).value;
+
+            // Limpa o datalist para evitar duplicatas
+            datalist.innerHTML = '';
 
             // Itera sobre a lista de moedas e adiciona opções ao elemento datalist
             moedas.forEach(moeda => {

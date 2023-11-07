d66c2535b7d202b36413fe2ce0c4324b26bda376
FranNinaa <francinesantoss85@gmail.com>
2023-11-06 21:28:58 -0300

    implementado a funcao calcular cotacao

--- Js/script.js
+++ Js/script.js
@@ -1,47 +1,84 @@
-// Função para carregar a lista de moedas (tanto origem quanto destino)
-function carregarMoedas(datalistId) {
-    // Obtém o elemento de entrada (moedaOrigemInput ou moedaDestinoInput) e o elemento datalist correspondente
-    let inputElement = document.getElementById(datalistId);
-    let datalist = document.getElementById(datalistId);
+        // Função para carregar a lista de moedas (tanto origem quanto destino)
+        function carregarMoedas(datalistId) {
+            // Obtém o elemento de entrada (moedaOrigemInput ou moedaDestinoInput) e o elemento datalist correspondente
+            let inputElement = document.getElementById(datalistId);
+            let datalist = document.getElementById(datalistId);
 
-    // Cria uma nova solicitação XMLHttpRequest
-    let xhr = new XMLHttpRequest();
+            // Cria uma nova solicitação XMLHttpRequest
+            let xhr = new XMLHttpRequest();
 
-    // Abre uma solicitação GET para a API do Banco Central (Note que você pode enfrentar problemas com chamadas de API para outro domínio)
-    xhr.open('GET', 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json&$select=simbolo,nomeFormatado');
+            // Abre uma solicitação GET para a API do Banco Central (Note que você pode enfrentar problemas com chamadas de API para outro domínio)
+            xhr.open('GET', 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json&$select=simbolo,nomeFormatado');
 
-    // Adiciona um evento para lidar com a resposta da solicitação
-    xhr.addEventListener('load', function () {
-        // Verifica se a resposta da solicitação foi bem-sucedida (status 200)
-        if (xhr.status === 200) {
-            // Analisa os dados JSON da resposta
-            let moedas = JSON.parse(xhr.responseText).value;
+            // Adiciona um evento para lidar com a resposta da solicitação
+            xhr.addEventListener('load', function () {
+                // Verifica se a resposta da solicitação foi bem-sucedida (status 200)
+                if (xhr.status === 200) {
+                    // Analisa os dados JSON da resposta
+                    let moedas = JSON.parse(xhr.responseText).value;
 
-            // Limpa o datalist para evitar duplicatas
-            datalist.innerHTML = '';
+                    // Limpa o datalist para evitar duplicatas
+                    datalist.innerHTML = '';
 
-            // Itera sobre a lista de moedas e adiciona opções ao elemento datalist
-            moedas.forEach(moeda => {
-                let option = document.createElement('option');
-                option.value = moeda.simbolo + ' (' + moeda.nomeFormatado + ')';
-                datalist.appendChild(option);
+                    // Itera sobre a lista de moedas e adiciona opções ao elemento datalist
+                    moedas.forEach(moeda => {
+                        let option = document.createElement('option');
+                        option.value = moeda.simbolo + ' (' + moeda.nomeFormatado + ')';
+                        datalist.appendChild(option);
+                    });
+                }
             });
+            // Envia a solicitação para obter a lista de moedas
+            xhr.send();
         }
-    });
-    // Envia a solicitação para obter a lista de moedas
-    xhr.send();
 
+        // Função para calcular a cotação
+        function calcularCotacao() {
+            let moedaOrigemInput = document.getElementById("moedaOrigemInput");
+            let moedaDestinoInput = document.getElementById("moedaDestinoInput");
+            let valorConverterInput = document.getElementById("valorConverterInput");
+            let compraRadio = document.getElementById("compraRadio");
+            let vendaRadio = document.getElementById("vendaRadio");
+            let saidaTextArea = document.getElementById("saidaTextArea");
 
-}
-// Cria uma instância do objeto Date para obter a data e hora atual
-const dataHoraAtual = new Date();
+            // Obtenha os valores selecionados
+            let moedaOrigem = moedaOrigemInput.value;
+            let moedaDestino = moedaDestinoInput.value;
+            let valor = valorConverterInput.value;
+            let tipoCotacao = compraRadio.checked ? "compra" : "venda";
 
-// Obtenha a data atual no formato "dd/mm/yyyy"
-const dataAtual = dataHoraAtual.toLocaleDateString();
+            // Faça uma solicitação XMLHttpRequest para obter a cotação
+            let xhr = new XMLHttpRequest();
 
-// Obtenha a hora atual no formato "hh:mm:ss"
-const horaAtual = dataHoraAtual.toLocaleTimeString();
+            xhr.open('GET', 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia' +
+                `(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='${moedaOrigem}'&@dataCotacao='${dataFormatada}'&` +
+                `$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,tipoBoletim`);
 
-// Exiba a data e hora no console ou em algum elemento HTML
-console.log("Data atual: " + dataAtual);
-console.log("Hora atual: " + horaAtual);
+            xhr.addEventListener('load', function () {
+                if (xhr.status === 200) {
+                    let cotacaoData = JSON.parse(xhr.responseText).value;
+
+                    // Encontre o valor de compra ou venda com base no tipoCotacao
+                    let cotacao = tipoCotacao === "compra" ? cotacaoData.cotacaoCompra : cotacaoData.cotacaoVenda;
+
+                    // Realize o cálculo da conversão
+                    let resultado = valor * cotacao;
+
+                    // Exiba o resultado na textarea
+                    saidaTextArea.value = `Valor convertido: ${resultado} ${moedaDestino}`;
+                } else {
+                    saidaTextArea.value = "Erro ao obter a cotação.";
+                }
+            });
+
+            xhr.send();
+        }
+
+        var dataAtual = new Date();
+        var dia = dataAtual.getDate();
+        var mes = dataAtual.getMonth() + 1;
+        var ano = dataAtual.getFullYear();
+        var dataFormatada = dia + '-' + mes + '-' + ano;
+
+        // Adicione o evento de clique ao botão "Converter"
+        document.getElementById("convertButton").addEventListener("click", calcularCotacao);
